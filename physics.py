d537ddd0ec8198b49911e5e6aeaba60d802e17ea
Toru <toorumon@gmail.com>
2019-02-25 15:50:22 -0500

    Added comments to physics engine

--- physics.py
+++ physics.py
@@ -32,6 +32,14 @@ dt = 0.001
 
 
 def init(n_body, fea_num, orbit):
+	"""
+	Initialization on just the first time step; fill all other time steps with 0's
+
+	:param n_body: number of objects
+	:param fea_num: number of features
+	:param orbit: whether simulate planet orbit
+	:return: a numpy vector of shape (ts, n_body, fea_num)
+	"""
 	data = np.zeros((ts, n_body, fea_num), dtype=float)
 	if orbit:
 		data[0][0][0] = 100
@@ -51,14 +59,14 @@ def init(n_body, fea_num, orbit):
 	# data[0][i][4]=np.random.rand()*10.0-5.0
 	else:
 		for i in range(n_body):
-			data[0][i][0] = np.random.rand() * 8.98 + 0.02
+			data[0][i][0] = np.random.rand() * 8.98 + 0.02  # mass
 			distance = np.random.rand() * 90.0 + 10.0
 			theta = np.random.rand() * 360
 			theta_rad = pi / 2 - radians(theta)
-			data[0][i][1] = distance * cos(theta_rad)
-			data[0][i][2] = distance * sin(theta_rad)
-			data[0][i][3] = np.random.rand() * 6.0 - 3.0
-			data[0][i][4] = np.random.rand() * 6.0 - 3.0
+			data[0][i][1] = distance * cos(theta_rad)  # x pos
+			data[0][i][2] = distance * sin(theta_rad)  # y pos
+			data[0][i][3] = np.random.rand() * 6.0 - 3.0  # x vel
+			data[0][i][4] = np.random.rand() * 6.0 - 3.0  # y vel
 	return data
 
 
@@ -67,7 +75,11 @@ def norm(x):
 
 
 def get_f(receiver, sender):
-	diff = sender[1:3] - receiver[1:3]
+	"""
+	Return gravitational force between two bodies (in vector form).
+	F = G*m1*m2 / r**2
+	"""
+	diff = sender[1:3] - receiver[1:3]  # difference in (x, y)
 	distance = norm(diff)
 	if distance < 1:
 		distance = 1
@@ -75,6 +87,11 @@ def get_f(receiver, sender):
 
 
 def calc(cur_state, n_body):
+	"""
+	Given current states of n objects, calculate their next states.
+
+	:return: a numpy vector of shape (n_body, num_features)
+	"""
 	next_state = np.zeros((n_body, num_features), dtype=float)
 	f_mat = np.zeros((n_body, n_body, 2), dtype=float)
 	f_sum = np.zeros((n_body, 2), dtype=float)
@@ -82,11 +99,12 @@ def calc(cur_state, n_body):
 	for i in range(n_body):
 		for j in range(i + 1, n_body):
 			if j != i:
+				# i is receiver, j is sender
 				f = get_f(cur_state[i][:3], cur_state[j][:3])
 				f_mat[i, j] += f
 				f_mat[j, i] -= f
 		f_sum[i] = np.sum(f_mat[i], axis=0)
-		acc[i] = f_sum[i] / cur_state[i][0]
+		acc[i] = f_sum[i] / cur_state[i][0]  # F = ma
 		next_state[i][0] = cur_state[i][0]
 		next_state[i][3:5] = cur_state[i][3:5] + acc[i] * dt
 		next_state[i][1:3] = cur_state[i][1:3] + next_state[i][3:5] * dt
@@ -94,8 +112,10 @@ def calc(cur_state, n_body):
 
 
 def gen(n_body, orbit):
-	# initialization on just first state
+	# initialize the first time step
 	d = init(n_body, num_features, orbit)
+
+	# calculate data for remaining time steps
 	for i in range(1, ts):
 		d[i] = calc(d[i - 1], n_body)
 	return d
@@ -120,5 +140,5 @@ def make_video(xy, filename):
 
 if __name__ == '__main__':
 	data = gen(3, True)
-	xy = data[:, :, 1:3]
+	xy = data[:, :, 1:3]  # x, y positions
 	make_video(xy, "test.mp4")
