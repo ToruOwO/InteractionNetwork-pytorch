8a334c20735f192f190dd9460f8b78f8901345a0
Toru <toorumon@gmail.com>
2019-02-25 11:05:19 -0500

    added physics engine

--- model.py
+++ model.py
@@ -1,30 +1,71 @@
 import torch
 import torch.nn as nn
 
+# constants
+
+n_objects = 3
+obj_dim = 7  # mass, x pos, y pos, x speed, y speed
+
+n_relations = n_objects * (n_objects - 1)
+rel_dim = 1
+
+eff_dim = 100
+hidden_obj_dim = 100
+hidden_rel_dim = 100
+
+
 class RelationModel(nn.Module):
-	def __init__(self):
+	def __init__(self, input_size, hidden_size, output_size):
 		super(RelationModel, self).__init__()
-		pass
+
+		self.model = nn.Sequential(
+			nn.Linear(input_size, hidden_size),
+			nn.ReLU(),
+			nn.Linear(hidden_size, hidden_size),
+			nn.ReLU(),
+			nn.Linear(hidden_size, hidden_size),
+			nn.ReLU(),
+			nn.Linear(hidden_size, output_size),
+			nn.ReLU()
+		)
 
 	def forward(self, x):
-		pass
+		'''
+        Args:
+            x: [n_relations, input_size]
+        Returns:
+            [n_relations, output_size]
+        '''
+		return self.model(x)
 
 
 class ObjectModel(nn.Module):
-	"""docstring for ObjectModel"""
-	def __init__(self, arg):
+	def __init__(self, input_size, hidden_size, output_size):
 		super(ObjectModel, self).__init__()
-		pass
+
+		self.model = nn.Sequential(
+			nn.Linear(input_size, hidden_size),
+			nn.ReLU(),
+			nn.Linear(hidden_size, output_size)
+		)
 
 	def forward(self, x):
-		pass
+		'''
+        Args:
+            x: [n_objects, input_size]
+        Returns:
+            [n_objects, output_size]
+
+        Note: output_size = number of states we want to predict
+        '''
+		return self.model(x)
 
 
 class InteractionNetwork(nn.Module):
-	def __init__(self, n_obj, n_rel, dim_obj, dim_rel, dim_eff):
+	def __init__(self, n_obj, n_rel, dim_obj, dim_rel, dim_eff, dim_hidden_obj, dim_hidden_rel):
 		super(InteractionNetwork, self).__init__()
-		self.rm = RelationModel()
-		self.om = ObjectModel()
+		self.rm = RelationModel(dim_obj * 2 + dim_rel, dim_hidden_rel, dim_eff)
+		self.om = ObjectModel(dim_obj + dim_eff, dim_hidden_obj, 3)  # x, y, z
 
 	def m(self, obj, rr, rs, ra):
 		orr = obj.mm(rr)
@@ -39,9 +80,13 @@ class InteractionNetwork(nn.Module):
 		e = self.rm(b)
 
 		# effect aggregator
-		a = torch.cat([obj, x, e.mm(rr.T)])
+		a = torch.cat([obj, x, e.mm(rr.t())])
 
 		# object module
 		p = self.om(c)
 
 		return p
+
+
+interaction_network = \
+	InteractionNetwork(n_objects, n_relations, obj_dim, rel_dim, eff_dim, hidden_obj_dim, hidden_rel_dim)
---
+++ physics.py
@@ -0,0 +1,124 @@
+"""
+Credit to jaesik817 for the physics engine.
+Original implementation:
+https://github.com/jaesik817/Interaction-networks_tensorflow/blob/master/physics_engine.py
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
+from math import cos, pi, radians, sin
+
+import matplotlib
+import numpy as np
+
+matplotlib.use("Agg")
+import matplotlib.pyplot as plt
+import matplotlib.animation as manimation
+
+# number of time steps
+ts = 1000
+
+# number of features, [mass, x, y, vx, vy]
+num_features = 5
+
+# G
+G = 10 ** 5
+
+# length of each time interval
+dt = 0.001
+
+
+def init(n_body, fea_num, orbit):
+	data = np.zeros((ts, n_body, fea_num), dtype=float)
+	if orbit:
+		data[0][0][0] = 100
+		data[0][0][1:5] = 0.0
+		for i in range(1, n_body):
+			data[0][i][0] = np.random.rand() * 8.98 + 0.02
+			distance = np.random.rand() * 90.0 + 10.0
+			theta = np.random.rand() * 360
+			theta_rad = pi / 2 - radians(theta)
+			data[0][i][1] = distance * cos(theta_rad)
+			data[0][i][2] = distance * sin(theta_rad)
+			data[0][i][3] = -1 * data[0][i][2] / norm(data[0][i][1:3]) * (
+					G * data[0][0][0] / norm(data[0][i][1:3]) ** 2) * distance / 1000
+			data[0][i][4] = data[0][i][1] / norm(data[0][i][1:3]) * (
+					G * data[0][0][0] / norm(data[0][i][1:3]) ** 2) * distance / 1000
+	# data[0][i][3]=np.random.rand()*10.0-5.0
+	# data[0][i][4]=np.random.rand()*10.0-5.0
+	else:
+		for i in range(n_body):
+			data[0][i][0] = np.random.rand() * 8.98 + 0.02
+			distance = np.random.rand() * 90.0 + 10.0
+			theta = np.random.rand() * 360
+			theta_rad = pi / 2 - radians(theta)
+			data[0][i][1] = distance * cos(theta_rad)
+			data[0][i][2] = distance * sin(theta_rad)
+			data[0][i][3] = np.random.rand() * 6.0 - 3.0
+			data[0][i][4] = np.random.rand() * 6.0 - 3.0
+	return data
+
+
+def norm(x):
+	return np.sqrt(np.sum(x ** 2))
+
+
+def get_f(receiver, sender):
+	diff = sender[1:3] - receiver[1:3]
+	distance = norm(diff)
+	if distance < 1:
+		distance = 1
+	return G * receiver[0] * sender[0] / (distance ** 3) * diff
+
+
+def calc(cur_state, n_body):
+	next_state = np.zeros((n_body, num_features), dtype=float)
+	f_mat = np.zeros((n_body, n_body, 2), dtype=float)
+	f_sum = np.zeros((n_body, 2), dtype=float)
+	acc = np.zeros((n_body, 2), dtype=float)
+	for i in range(n_body):
+		for j in range(i + 1, n_body):
+			if j != i:
+				f = get_f(cur_state[i][:3], cur_state[j][:3])
+				f_mat[i, j] += f
+				f_mat[j, i] -= f
+		f_sum[i] = np.sum(f_mat[i], axis=0)
+		acc[i] = f_sum[i] / cur_state[i][0]
+		next_state[i][0] = cur_state[i][0]
+		next_state[i][3:5] = cur_state[i][3:5] + acc[i] * dt
+		next_state[i][1:3] = cur_state[i][1:3] + next_state[i][3:5] * dt
+	return next_state
+
+
+def gen(n_body, orbit):
+	# initialization on just first state
+	d = init(n_body, num_features, orbit)
+	for i in range(1, ts):
+		d[i] = calc(d[i - 1], n_body)
+	return d
+
+
+def make_video(xy, filename):
+	os.system("rm -rf pics/*")
+	FFMpegWriter = manimation.writers['ffmpeg']
+	metadata = dict(title='Movie Test', artist='Matplotlib', comment='Movie support!')
+	writer = FFMpegWriter(fps=15, metadata=metadata)
+	fig = plt.figure()
+	plt.xlim(-200, 200)
+	plt.ylim(-200, 200)
+	fig_num = len(xy)
+	color = ['ro', 'bo', 'go', 'ko', 'yo', 'mo', 'co']
+	with writer.saving(fig, filename, fig_num):
+		for i in range(fig_num):
+			for j in range(len(xy[0])):
+				plt.plot(xy[i, j, 1], xy[i, j, 0], color[j % len(color)])
+			writer.grab_frame()
+
+
+if __name__ == '__main__':
+	data = gen(3, True)
+	xy = data[:, :, 1:3]
+	make_video(xy, "test.mp4")
